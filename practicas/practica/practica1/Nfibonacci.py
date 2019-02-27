7f4c6c1083555a283e7470ee53219205d8ec8832
Margmt <47332861+Margmt@users.noreply.github.com>
2019-02-27 13:36:56 +0000

    Add files via upload

---
+++ practicas/practica/practica1/Nfibonacci.py
@@ -0,0 +1,12 @@
+a=0
+b=1
+print (a)
+print (b)
+i=0
+n=100
+while i<n:
+	s=a+b
+	a=b
+	b=s
+	print (s)
+	i=i+1
---
+++ practicas/practica/practica1/Nprimerosysuma.py
@@ -0,0 +1,12 @@
+sum=0
+Num=int(input("ingrese un numero"))
+for i in range(1, Num+1):
+	sum=sum+i
+print(sum)
+
+print(range(0,Num)) 
+
+i = 1
+while i <= 10:
+	print(i)
+	i = i+1
